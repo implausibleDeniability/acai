8f0b0ddf7e0cc35742bc8993a35f3bc63fef9561
Maxim Faleev <m.faleev@innopolis.university>
2023-10-26 08:30:15 +0300

    move `generate batch` to file

--- acai/data.py
+++ acai/data.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import torch
     
 
 IMAGE_SIZE = 32
 
 def make_line_image(angle: float) -> np.ndarray:
+    """Creates grayscale image of slope with `angle`"""
     DRAWING_IMAGE_SIZE = 256
     
     startpoint = (0.5, 0.5)
@@ -17,3 +19,16 @@ def make_line_image(angle: float) -> np.ndarray:
     image = cv2.line(image, absolute_startpoint, absolute_endpoint, color=255, thickness=12)
     image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
     return image
+
+
+PI = 3.1416
+
+def generate_batch(batchsize: int) -> torch.Tensor:
+    """Generates torch-compliant batch of images of lines"""
+    angles = np.random.uniform(-PI, PI, size=batchsize)
+    images = [make_line_image(angle) for angle in angles]
+    batch = np.stack(images)
+    batch = batch / 255
+    batch = torch.FloatTensor(batch)
+    batch = batch.view(batchsize, 1, IMAGE_SIZE, IMAGE_SIZE)
+    return batch
